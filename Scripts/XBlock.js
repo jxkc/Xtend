d490419f390d3d260b0b6663ce13ba12be840470
Jack <113445858+jxkc@users.noreply.github.com>
2023-03-13 18:13:52 +0000

    I hate Typescript.

--- Scripts/XBlock.js
+++ Scripts/XBlock.js
@@ -1,8 +1,6 @@
 //This is the code for the 'XBlocks'
-
 //Needs unique generator
 //Needs to be able to create div element
-
 /*Creating Div Element -
     1. Fixed Div Size - (w 200 x h 400)
     2. Need to create Elements
@@ -19,22 +17,21 @@
 
 // Function to generate a Unique String from a selection of Upper and lower case letters and numbers.
 // Requires a number to be passed in to determine the length required.
-function UniqueID(length: number): string {
-    let result: string = '';
-
+function UniqueID(length) {
+    var result = '';
+    
     //characters defines the characters being used for the String
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     //const charactersLength = characters.length;
-
-    let counter: number = 0;
-
+    var counter = 0;
     // While the counter is less than the length
     //  Iterate through loop adding random chars to the result.
-    while(counter < length){
+    while (counter < length) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
         counter += 1;
     }
-
     //returning the result.
     return result;
-};
+}
+;
+console.log(UniqueID(8));
